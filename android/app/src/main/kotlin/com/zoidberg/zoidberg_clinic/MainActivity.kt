1cee3a72e1effd1ad627282b92776d9d8973ea4f
antoc <antocapadona7@gmail.com>
2025-08-20 21:58:27 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/zoidberg/zoidberg_clinic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.zoidberg.zoidberg_clinic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Pequeña verificación en consola
+  print('Firebase apps: ${Firebase.apps.map((a) => a.name).toList()}');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Firebase inicializado 🦞')),
+      ),
+    );
+  }
+}
